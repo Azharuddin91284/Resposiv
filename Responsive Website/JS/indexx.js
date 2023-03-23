7a650079c6ede462c6135768540cb723c7dc999a
Azharuddin91284 <115046468+Azharuddin91284@users.noreply.github.com>
2023-03-24 01:41:51 +0530

    Add files via upload

---
+++ Responsive Website/JS/indexx.js
@@ -0,0 +1,9 @@
+var a=786;
+var b=" Azharuddin Ansari";
+var sum=a+b;
+console.log(sum);
+console.log(typeof sum);
+for ( i=0;i<10;i++){
+    console.log("Azharuddin Ansari");
+
+}
---
+++ Responsive Website/JS/script.js
@@ -0,0 +1,14 @@
+ burger = document.querySelector('.burger');
+burger=document.querySelector('.navbar');
+burger=document.querySelector('.rightnav');
+burger=document.querySelector('.nav-list');
+
+
+
+burger.addEventListener('click', ()=>{
+    rightnav.classList.toggle('v-class');
+    nav-list.classList.toggle('v-class');
+    navbar.classList.toggle('h-nav');
+
+
+})
